79529a387c90a788580f3d5b42577aaed933c99c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-15 22:33:57 +0200

    add sink interface for custom user implementations

---
+++ src/Cayd.AspNetCore.FlexLog/Sinks/IFlexLogSink.cs
@@ -0,0 +1,11 @@
+﻿using Cayd.AspNetCore.FlexLog.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cayd.AspNetCore.FlexLog.Sinks
+{
+    public interface IFlexLogSink
+    {
+        Task FlushAsync(IReadOnlyCollection<FlexLogContext> buffer);
+    }
+}
