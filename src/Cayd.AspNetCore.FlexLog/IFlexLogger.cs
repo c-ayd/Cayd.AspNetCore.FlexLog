67005997b682750c7d0d0ef2a73c4998a4ea874f
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-15 22:08:20 +0200

    add main flex log interface for dependency injection

---
+++ src/Cayd.AspNetCore.FlexLog/IFlexLogger.cs
@@ -0,0 +1,23 @@
+﻿using Cayd.AspNetCore.FlexLog.Enums;
+using System;
+
+namespace Cayd.AspNetCore.FlexLog
+{
+    public interface IFlexLogger<T>
+    {
+        void Log(ELogLevel logLevel, string message, object? metadata = null);
+        void Log(ELogLevel logLevel, string message, Exception exception, object? metadata = null);
+        void LogTrace(string message, object? metadata = null);
+        void LogTrace(string message, Exception exception, object? metadata = null);
+        void LogDebug(string message, object? metadata = null);
+        void LogDebug(string message, Exception exception, object? metadata = null);
+        void LogInformation(string message, object? metadata = null);
+        void LogInformation(string message, Exception exception, object? metadata = null);
+        void LogWarning(string message, object? metadata = null);
+        void LogWarning(string message, Exception exception, object? metadata = null);
+        void LogError(string message, object? metadata = null);
+        void LogError(string message, Exception exception, object? metadata = null);
+        void LogCritical(string message, object? metadata = null);
+        void LogCritical(string message, Exception exception, object? metadata = null);
+    }
+}
