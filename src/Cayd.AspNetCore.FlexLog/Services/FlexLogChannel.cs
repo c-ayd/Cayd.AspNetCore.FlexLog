abcbb648dfb8b47718e8f2471f65323dc0d02ecb
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-15 22:34:44 +0200

    add log channel to store log contexts and sinks

---
+++ src/Cayd.AspNetCore.FlexLog/Services/FlexLogChannel.cs
@@ -0,0 +1,27 @@
+﻿using Cayd.AspNetCore.FlexLog.Logging;
+using Cayd.AspNetCore.FlexLog.Sinks;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace Cayd.AspNetCore.FlexLog.Services
+{
+    public class FlexLogChannel
+    {
+        public Channel<FlexLogContext> Logs { get; private set; }
+
+        private readonly List<IFlexLogSink> _sinks;
+        public IReadOnlyList<IFlexLogSink> Sinks => _sinks;
+
+        public FlexLogChannel(ICollection<IFlexLogSink> sinks)
+        {
+            Logs = Channel.CreateUnbounded<FlexLogContext>(new UnboundedChannelOptions()
+            {
+                AllowSynchronousContinuations = false,
+                SingleWriter = false,
+                SingleReader = true
+            });
+
+            _sinks = new List<IFlexLogSink>(sinks);
+        }
+    }
+}
