06a11a28be1507333c37ee991485c25d03bb0ac4
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-16 16:58:21 +0200

    add middleware extension method for logging

---
+++ src/Cayd.AspNetCore.FlexLog/Extensions/UseFlexLog.cs
@@ -0,0 +1,13 @@
+﻿using Cayd.AspNetCore.FlexLog.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Cayd.AspNetCore.FlexLog.Extensions
+{
+    public static partial class FlexLogExtensions
+    {
+        public static void UseFlexLog(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<FlexLogMiddleware>();
+        }
+    }
+}
